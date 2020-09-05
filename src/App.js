bef54ab9648914ff6df1b42fe601a1cc06b5248a
Pedro Henrique <phmstulp@minha.fag.edu.br>
2020-09-05 16:21:42 -0300

    Aparece a tabela e nao carrega o JSON

--- src/App.js
+++ src/App.js
@@ -1,26 +1,71 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import MaterialTable from 'material-table';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function componentDidMount() {
+  axios.get(`https://jsonplaceholder.typicode.com/users`)
+    .then(res => {
+      return res.data;
+    }).catch(err => {
+      console.log("Error Reading data " + err);
+    });
 }
 
-export default App;
+export default function MaterialTableDemo() {
+  const [state, setState] = React.useState({
+    columns: [
+      { title: 'ID ', field: 'id' },
+      { title: 'Nome', field: 'name' },
+      { title: 'Usuário', field: 'username' },
+      { title: 'E-mail', field: 'email' },
+      { title: 'Telefone', field: 'phone' },
+      { title: 'Website', field: 'website' },
+    ],
+    data: componentDidMount(),
+  });
+
+  return (
+    <MaterialTable
+      title="Usuários"
+      columns={state.columns}
+      data={state.data}
+      editable={{
+        onRowAdd: (newData) =>
+          new Promise((resolve) => {
+            setTimeout(() => {
+              resolve();
+              setState((prevState) => {
+                const data = [...prevState.data];
+                data.push(newData);
+                return { ...prevState, data };
+              });
+            }, 600);
+          }),
+        onRowUpdate: (newData, oldData) =>
+          new Promise((resolve) => {
+            setTimeout(() => {
+              resolve();
+              if (oldData) {
+                setState((prevState) => {
+                  const data = [...prevState.data];
+                  data[data.indexOf(oldData)] = newData;
+                  return { ...prevState, data };
+                });
+              }
+            }, 600);
+          }),
+        onRowDelete: (oldData) =>
+          new Promise((resolve) => {
+            setTimeout(() => {
+              resolve();
+              setState((prevState) => {
+                const data = [...prevState.data];
+                data.splice(data.indexOf(oldData), 1);
+                return { ...prevState, data };
+              });
+            }, 600);
+          }),
+      }}
+    />
+  );
+}
