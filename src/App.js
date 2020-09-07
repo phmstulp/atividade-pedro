fa36b945af47fd5085f385f7b5fbe0c242bfdd02
Pedro Henrique <phmstulp@minha.fag.edu.br>
2020-09-07 19:13:08 -0300

    Deu certo carai

--- src/App.js
+++ src/App.js
@@ -1,77 +1,102 @@
-import React, { useEffect } from 'react';
-import MaterialTable from 'material-table';
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TablePagination from '@material-ui/core/TablePagination';
+import TableRow from '@material-ui/core/TableRow';
 import axios from 'axios';
+import { useState, useEffect } from 'react';
 
-export default function MaterialTableDemo() {
-  useEffect(() => {
-    componentDidMount();
-  });
+const columns = [
+  { id: 'id', label: 'ID', minWidth: 100 },
+  { id: 'name', label: 'Nome', minWidth: 100 },
+  { id: 'username', label: 'usuário', minWidth: 100 },
+  { id: 'email', label: 'E-mail', minWidth: 100 },
+  { id: 'phone', label: 'Telefone', minWidth: 100 },
+  { id: 'website', label: 'Website', minWidth: 100 },
+];
 
-  const [state, setState] = React.useState({
-    columns: [
-      { title: 'ID ', field: 'id' },
-      { title: 'Nome', field: 'name' },
-      { title: 'Usuário', field: 'username' },
-      { title: 'E-mail', field: 'email' },
-      { title: 'Telefone', field: 'phone' },
-      { title: 'Website', field: 'website' },
-    ],
-    data: [],
-  });
+const useStyles = makeStyles({
+  root: {
+    width: '100%',
+  },
+  container: {
+    maxHeight: 440,
+  },
+});
 
-  function componentDidMount() {
-    axios.get(`https://jsonplaceholder.typicode.com/users`)
-      .then((res) => {
-        const users = res.data;
-        state.data = users;
-        return;
-      }).catch(err => {
-        console.log("Error Reading data " + err);
-      });
+export default function StickyHeadTable() {
+  const classes = useStyles();
+  const [page, setPage] = React.useState(0);
+  const [data, setData] = useState([]);
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+
+  useEffect(() => {    
+    const GetData = async () => {    
+      const result = await axios.get('https://jsonplaceholder.typicode.com/users');
+      setData(result.data);    
+    }  
+    GetData();    
+    console.log(data);  
+  }, []);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
   };
 
   return (
-    <MaterialTable
-      title="Usuários"
-      columns={state.columns}
-      data={state.data}
-      editable={{
-        onRowAdd: (newData) =>
-          new Promise((resolve) => {
-            setTimeout(() => {
-              resolve();
-              setState((prevState) => {
-                const data = [...prevState.data];
-                data.push(newData);
-                return { ...prevState, data };
-              });
-            }, 600);
-          }),
-        onRowUpdate: (newData, oldData) =>
-          new Promise((resolve) => {
-            setTimeout(() => {
-              resolve();
-              if (oldData) {
-                setState((prevState) => {
-                  const data = [...prevState.data];
-                  data[data.indexOf(oldData)] = newData;
-                  return { ...prevState, data };
-                });
-              }
-            }, 600);
-          }),
-        onRowDelete: (oldData) =>
-          new Promise((resolve) => {
-            setTimeout(() => {
-              resolve();
-              setState((prevState) => {
-                const data = [...prevState.data];
-                data.splice(data.indexOf(oldData), 1);
-                return { ...prevState, data };
-              });
-            }, 600);
-          }),
-      }}
-    />
+    <Paper className={classes.root}>
+      <TableContainer className={classes.container}>
+        <Table stickyHeader aria-label="sticky table">
+          <TableHead>
+            <TableRow>
+              {columns.map((column) => (
+                <TableCell
+                  key={column.id}
+                  align={column.align}
+                  style={{ minWidth: column.minWidth }}
+                >
+                  {column.label}
+                </TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {data.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => {
+              return (
+                <TableRow hover role="checkbox" tabIndex={-1} key={row.id}>
+                  {columns.map((column) => {
+                    const value = row[column.id];
+                    return (
+                      <TableCell key={column.id} align={column.align}>
+                        {column.format && typeof value === 'number' ? column.format(value) : value}
+                      </TableCell>
+                    );
+                  })}
+                </TableRow>
+              );
+            })}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <TablePagination
+        rowsPerPageOptions={[5, 10, 25]}
+        component="div"
+        count={data.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onChangePage={handleChangePage}
+        onChangeRowsPerPage={handleChangeRowsPerPage}
+      />
+    </Paper>
   );
 }
